1ed0202a55946b160b1d17ce69828add74ca39b0
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-11 23:03:10 +0100

    function signatures

--- public/script.js
+++ public/script.js
@@ -1,8 +1,32 @@
 "use strict";
-/**Type alisases help us reduce the amount of type duplication when trying to declare types */
-const logDetails = (uid, item) => {
-    console.log(`${item} has a userId of ${uid}`);
+/*
+let greet: Function
+above we are stating that the greet variable will hold only functions but we can go a bit further and state what
+kind of structure the function will have.
+
+e.g () => void
+the above is a function signature that states that this fucntion holds no argument and returns void(doesnt return anything)
+*/
+//example 1 of a function signature
+let greet; //this is a structure/signature
+greet = (name, greet) => {
+    console.log(`${name} says ${greet}`);
 };
-const greet = (user) => {
-    console.log(`Hello ${user.name} `);
+greet("yemi", "hello");
+//example 2 of function signature
+let calc; //this is a structure/signature
+calc = (numOne, nummTwo, action) => {
+    if (action === "add") {
+        return numOne + nummTwo;
+    }
+    else {
+        return numOne - nummTwo;
+    }
 };
+console.log(calc(1, 2, "add"));
+//example 3 of function signature
+let details;
+details = (Joseph) => {
+    console.log(`${Joseph.name} is ${Joseph.age} years old`);
+};
+details({ name: "yemi", age: 10 });
--- src/script.ts
+++ src/script.ts
@@ -1,29 +1,46 @@
-/**Type alisases help us reduce the amount of type duplication when trying to declare types */
-
-
-
 /*
-Examples
+let greet: Function 
+above we are stating that the greet variable will hold only functions but we can go a bit further and state what
+kind of structure the function will have.
 
-const logDetails = (uid:string|number,item:string)=>{
-    console.log(`${item} has a userId of ${uid}`)
-}
-
-const greet = (user:{name:string,uid:string|number}) => {
-console.log(`Hello ${user.name} `)
-}
+e.g () => void
+the above is a function signature that states that this fucntion holds no argument and returns void(doesnt return anything)
 */
 
-//to fix the commented examples above we can reduce the amount of repeated types by declaring a variable that holds types
+//example 1 of a function signature
 
-type stringOrNum = string|number
+let greet:(a:string,b:string) => void //this is a structure/signature
 
-const logDetails = (uid:stringOrNum,item:string)=>{ // we put our custom type as an argument
-    console.log(`${item} has a userId of ${uid}`)
+greet = (name:string, greet:string ) => {//here the function follows the signature
+    console.log(`${name} says ${greet}`)
 }
 
-type objWithName = {name:string,uid:stringOrNum}
 
-const greet = (user:objWithName) => {
-    console.log(`Hello ${user.name} `)
+greet("yemi","hello")
+
+
+//example 2 of function signature
+
+let calc: (a:number,b:number,c:string) => number //this is a structure/signature
+
+calc = (numOne:number,nummTwo:number,action:string) => {//here the function follows the signature
+ if(action === "add"){
+     return numOne + nummTwo
+ }else{
+     return numOne - nummTwo
+ }
+
 }
+
+console.log(calc(1,2,"add"))
+
+//example 3 of function signature
+let details: (obj:{name:string, age:number}) => void
+
+type person = {name:string,age:number}
+
+details = (Joseph:person) => {
+    console.log(`${Joseph.name} is ${Joseph.age} years old`)
+}
+
+details({name:"yemi",age:10})
