b303729abb01e00882c09b0fcc1d5253c4c4b211
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-11 10:04:14 +0100

    explicitTypes Learning

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ var listOfHobbies;
 listOfHobbies = ["yemi", "omotosho", "Joseph"]; //i wunt get errors since i am assigning strings to the array 
 // console.log(listOfHobbies)
 // explicitly defining more than one dataType for an array in typescript
-var listOfCourse; //this allows you use mixed dataTypes in an array in typescript
+var listOfCourse; //This is called Union Type where you can use either one type or the other.this allows you use mixed dataTypes in an array in typescript
 listOfCourse = ["yemi", "omotosho", "Joseph", 9]; //wunt get an error coz i declared that i wanted this variable to hold an array of mixed dataType
+/**to initialize your declared string array with an empty array */
+var listOfSocials = []; //doing this will allow you create empty array you can start using
+listOfSocials.push("crowwe"); //i can only push data when i have an empty array
+// console.log(listOfSocials)
+//Union Types on normal variables
+var normal;
+normal = 3;
+normal = "true";
+//normal = true //this will give error
+//declaring objects
+var myData; // or let myData : {}
+myData = { myName: "joe", age: 44 };
+console.log(myData);
+//initializing structure an object should take.
+var myBag;
+myBag = {
+    isBlack: true,
+    content: 3,
+    name: "ko"
+};
