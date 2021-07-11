63ced9b9e02d37278b3dfb9836033c9560285a00
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-11 20:34:33 +0100

    config

---
+++ public/script.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("yemi");
+console.log("omotosho");
+console.log("jazz");
+let foo = function () {
+    console.log("foo");
+};
+console.log(foo());
---
+++ src/script.ts
@@ -0,0 +1,11 @@
+console.log("yemi")
+
+console.log("omotosho")
+
+console.log("jazz")
+
+let foo = function(){
+    console.log("foo")
+}
+
+console.log(foo())
