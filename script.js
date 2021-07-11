501b08a4f10d214078813462cfe2043fe4056f6c
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-11 20:03:30 +0100

    any keyword

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-/**Explicitly stating the types a variable should hold without assigning it a value */
-var age;
-var isTall;
+/*
+Dyanamic(Any) Types basically tells typeScript that a data type declared to a variable can be changed anytime in the future of the code
+and if the variable has a particular data type declared, we can change it, without getting error
+
+
+*/
+//using "any" keyword with primitives
 var myName;
-myName = "Joseph"; ///it allowed string hence no error
-//age = "kkk" //this will give an error coz the type assigned to age ought to be a number
-// explicitly defining the data type arrays will hold
-var listOfHobbies;
-listOfHobbies = ["yemi", "omotosho", "Joseph"]; //i wunt get errors since i am assigning strings to the array 
-// console.log(listOfHobbies)
-// explicitly defining more than one dataType for an array in typescript
-var listOfCourse; //This is called Union Type where you can use either one type or the other.this allows you use mixed dataTypes in an array in typescript
-listOfCourse = ["yemi", "omotosho", "Joseph", 9]; //wunt get an error coz i declared that i wanted this variable to hold an array of mixed dataType
-/**to initialize your declared string array with an empty array */
-var listOfSocials = []; //doing this will allow you create empty array you can start using
-listOfSocials.push("crowwe"); //i can only push data when i have an empty array
-// console.log(listOfSocials)
-//Union Types on normal variables
-var normal;
-normal = 3;
-normal = "true";
-//normal = true //this will give error
-//declaring objects
-var myData; // or let myData : {}
-myData = { myName: "joe", age: 44 };
+myName = "jOSEPH";
+console.log(myName);
+myName = 93;
+console.log(myName);
+myName = true;
+console.log(myName);
+//using "any" keyword with arrays
+var listMyNames = [];
+listMyNames.push("Joseph");
+console.log(listMyNames);
+listMyNames.push(89);
+console.log(listMyNames);
+listMyNames.push(true);
+console.log(listMyNames);
+//using "any" keyword with objects
+var myData;
+myData = { name: "joseph", age: 98, height: true };
 console.log(myData);
-//initializing structure an object should take.
-var myBag;
-myBag = {
-    isBlack: true,
-    content: 3,
-    name: "ko"
-};
--- script.ts
+++ script.ts
@@ -1,67 +1,47 @@
-/**Explicitly stating the types a variable should hold without assigning it a value */
-let age: number
-let isTall: boolean
-let myName:string
-
-myName = "Joseph" ///it allowed string hence no error
-
-//age = "kkk" //this will give an error coz the type assigned to age ought to be a number
+/*
+Dyanamic(Any) Types basically tells typeScript that a data type declared to a variable can be changed anytime in the future of the code
+and if the variable has a particular data type declared, we can change it, without getting error
 
 
-// explicitly defining the data type arrays will hold
-
-let listOfHobbies: (string)[]
-
-listOfHobbies = ["yemi","omotosho","Joseph"] //i wunt get errors since i am assigning strings to the array 
-
-// console.log(listOfHobbies)
-
-// explicitly defining more than one dataType for an array in typescript
-
-let listOfCourse: (string|number)[] //This is called Union Type where you can use either one type or the other.this allows you use mixed dataTypes in an array in typescript
-
-listOfCourse = ["yemi","omotosho","Joseph",9] //wunt get an error coz i declared that i wanted this variable to hold an array of mixed dataType
+*/
 
 
-/**to initialize your declared string array with an empty array */
-let listOfSocials: (string)[] = []  //doing this will allow you create empty array you can start using
+//using "any" keyword with primitives
 
-listOfSocials.push("crowwe") //i can only push data when i have an empty array
+let myName:any
 
-// console.log(listOfSocials)
+myName = "jOSEPH"
 
+console.log(myName)
 
-//Union Types on normal variables
-let normal: string | number 
+myName = 93
 
-normal = 3
+console.log(myName)
 
-normal = "true"
+myName = true
 
-//normal = true //this will give error
+console.log(myName)
 
+//using "any" keyword with arrays
 
+let listMyNames: any[] = []
 
-//declaring objects
+listMyNames.push("Joseph")
 
-let myData: object// or let myData : {}
+console.log(listMyNames)
 
+listMyNames.push(89)
 
-myData = {myName:"joe",age:44}
+console.log(listMyNames)
 
+listMyNames.push(true)
+
+console.log(listMyNames)
+
+//using "any" keyword with objects
+
+let myData: {name:any,age:any,height:any} 
+
+myData = {name:"joseph",age:98,height:true}
 console.log(myData)
 
-//initializing structure an object should take.
-
-let myBag :{
-    isBlack : boolean,
-    content: number,
-    name:string
-
-}
-
-myBag = {
-    isBlack:true,
-    content: 3,
-    name:"ko"
-}
