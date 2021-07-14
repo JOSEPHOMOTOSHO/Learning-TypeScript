75d78c51b259a6157b909ac3dfd392e46a7a3bd0
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-15 00:43:02 +0100

    interfaces are like type aliases for objects

--- public/script.js
+++ public/script.js
@@ -1,32 +1,9 @@
 "use strict";
-/*
-let greet: Function
-above we are stating that the greet variable will hold only functions but we can go a bit further and state what
-kind of structure the function will have.
-
-e.g () => void
-the above is a function signature that states that this fucntion holds no argument and returns void(doesnt return anything)
-*/
-//example 1 of a function signature
-let greet; //this is a structure/signature
-greet = (name, greet) => {
-    console.log(`${name} says ${greet}`);
+//Interfaces help us to create a structure for an object that we'll like to use 
+let calcRectangle = (options) => {
+    return (options.height * options.width);
 };
-greet("yemi", "hello");
-//example 2 of function signature
-let calc; //this is a structure/signature
-calc = (numOne, nummTwo, action) => {
-    if (action === "add") {
-        return numOne + nummTwo;
-    }
-    else {
-        return numOne - nummTwo;
-    }
-};
-console.log(calc(1, 2, "add"));
-//example 3 of function signature
-let details;
-details = (Joseph) => {
-    console.log(`${Joseph.name} is ${Joseph.age} years old`);
-};
-details({ name: "yemi", age: 10 });
+calcRectangle({
+    height: 78,
+    width: 980
+});
--- src/script.ts
+++ src/script.ts
@@ -1,46 +1,30 @@
-/*
-let greet: Function 
-above we are stating that the greet variable will hold only functions but we can go a bit further and state what
-kind of structure the function will have.
+//Interfaces help us to create a structure for an object that we'll like to use 
 
-e.g () => void
-the above is a function signature that states that this fucntion holds no argument and returns void(doesnt return anything)
+/* an interface can be declared like this using type aliases or custom type
+type RectangleOptions = {
+    height:number,
+    width:number
+}
+
+let calcRectangle = (options:RectangleOptions) => {
+ return (options.height * options.width)
+}
+
+console.log(calcRectangle({height:78,width:90}))
 */
 
-//example 1 of a function signature
+/* An interface can also be created using the interface keyword */
 
-let greet:(a:string,b:string) => void //this is a structure/signature
-
-greet = (name:string, greet:string ) => {//here the function follows the signature
-    console.log(`${name} says ${greet}`)
+interface RectangleOptions{
+    height:number,
+    width:number
 }
 
-
-greet("yemi","hello")
-
-
-//example 2 of function signature
-
-let calc: (a:number,b:number,c:string) => number //this is a structure/signature
-
-calc = (numOne:number,nummTwo:number,action:string) => {//here the function follows the signature
- if(action === "add"){
-     return numOne + nummTwo
- }else{
-     return numOne - nummTwo
- }
-
-}
-
-console.log(calc(1,2,"add"))
-
-//example 3 of function signature
-let details: (obj:{name:string, age:number}) => void
-
-type person = {name:string,age:number}
-
-details = (Joseph:person) => {
-    console.log(`${Joseph.name} is ${Joseph.age} years old`)
-}
-
-details({name:"yemi",age:10})
+let calcRectangle = (options:RectangleOptions) => {
+    return (options.height * options.width)
+   }
+   
+ calcRectangle({
+     height:78,
+     width:980
+    })
